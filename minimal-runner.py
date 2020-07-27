c59e7dc88e40b067acd2ebab6ca6e525056de404
Peter Law <PeterJCLaw@gmail.com>
2020-07-27 21:06:17 +0100

    Add a minimal runner script

---
+++ minimal-runner.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import argparse
+import io
+import pathlib
+import pprint
+import time
+import tokenize
+
+from flake8_multiline_containers import MultilineContainers
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='+', type=pathlib.Path)
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace) -> None:
+    start = time.time()
+
+    for path in args.files:
+        tokens = tokenize.tokenize(io.BytesIO(path.read_bytes()).readline)
+        checker = MultilineContainers(tokens=tokens)
+        pprint.pprint(list(checker))
+
+    print(time.time() - start)
+
+
+if __name__ == '__main__':
+    main(parse_args())
